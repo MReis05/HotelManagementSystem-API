1732677cde5d95164072a6f74bb5a38fa52a0dbe
Matheus Reis <matheusreiscrs@gmail.com>
2025-10-21 19:14:16 -0300

    first commit

---
+++ src/main/java/com/reis/HotelManagementSystem_APi/HotelManagementSystemAPiApplication.java
@@ -0,0 +1,13 @@
+package com.reis.HotelManagementSystem_APi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementSystemAPiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagementSystemAPiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reis/HotelManagementSystem_APi/HotelManagementSystemAPiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reis.HotelManagementSystem_APi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementSystemAPiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
